6440957bdbd64e5cfbd13d48e1527ec798963be9
heumm <hanheum97@gmail.com>
2019-11-14 15:33:17 +0900

    .

--- board/src/main/java/com/care/member_dao/MemberDAO.java
+++ board/src/main/java/com/care/member_dao/MemberDAO.java
@@ -1,7 +1,10 @@
 package com.care.member_dao;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class MemberDAO {
+	
+	@Autowired
 	SqlSession sqlSession;
 }
